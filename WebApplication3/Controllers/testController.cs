69801e805658f929feb24ba59c4fa493d107e9a1
Roman Dark <amaii.sakura@gmail.com>
2020-02-19 01:47:09 +0600

    Добавьте файлы проекта.

---
+++ WebApplication3/Controllers/testController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class testController : Controller
+    {
+        // GET: test
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public FileResult PartialTestView(string ObjectId)
+        {
+            var res = ObjectId;
+
+            throw new Exception();
+            
+            //return
+        }
+    }
+}
